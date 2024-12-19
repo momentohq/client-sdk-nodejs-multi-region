a8637a1fdf363a52f87c80c8827d4078508ac715
Michael Landis <michael@momentohq.com>
2024-12-19 15:03:58 -0800

    chore: update example program (#11)
    Updates example program to point to latest released version.

--- examples/multi-region.ts
+++ examples/multi-region.ts
@@ -1,23 +1,54 @@
-import {Configurations, CredentialProvider, MultiRegionCacheWriterClient} from '@gomomento-poc/sdk-nodejs-multi-region';
+import {
+  Configurations,
+  CredentialProvider,
+  MultiRegionCacheSetResponse,
+  MultiRegionCacheSortedSetPutElementsResponse,
+  MultiRegionCacheWriterClient,
+} from '@gomomento-poc/sdk-multi-region';
 
 async function main(): Promise<void> {
+  const cacheName = process.env.MOMENTO_CACHE_NAME ?? 'test-cache';
   const client = new MultiRegionCacheWriterClient({
     credentialProviders: {
-      'us-west-2': CredentialProvider.fromEnvVar('MOMENTO_API_KEY_US_WEST_2'),
-      'us-east-1': CredentialProvider.fromEnvVar('MOMENTO_API_KEY_US_EAST_1'),
+      'region-1': CredentialProvider.fromEnvVar('MOMENTO_API_KEY_REGION_1'),
+      'region-2': CredentialProvider.fromEnvVar('MOMENTO_API_KEY_REGION_2'),
     },
     configuration: Configurations.Laptop.latest(),
     defaultTtlSeconds: 60,
   });
-  const response = await client.set('test-cache', 'key', 'value');
-  console.log(response);
+
+  const setResponse = await client.set(cacheName, 'scalar', 'value');
+  switch (setResponse.type) {
+    case MultiRegionCacheSetResponse.Success:
+      console.log('Success:', setResponse.results());
+      break;
+    case MultiRegionCacheSetResponse.Error:
+      console.error(
+        `Found {${Object.keys(setResponse.errors()).length}} errors and {${
+          Object.keys(setResponse.successes()).length
+        }} successes. The full results are: ${setResponse.toString()}`
+      );
+      break;
+  }
+
+  const sortedSetResponse = await client.sortedSetPutElements(cacheName, 'sorted-set', {
+    element1: 1,
+    element2: 2,
+  });
+  switch (sortedSetResponse.type) {
+    case MultiRegionCacheSortedSetPutElementsResponse.Success:
+      console.log('Success:', sortedSetResponse.results());
+      break;
+    case MultiRegionCacheSortedSetPutElementsResponse.Error:
+      console.error(
+        `Found {${Object.keys(sortedSetResponse.errors()).length}} errors and {${
+          Object.keys(sortedSetResponse.successes()).length
+        }} successes. The full results are: ${setResponse.toString()}`
+      );
+      break;
+  }
 }
 
 main()
-  .then(() => {
-    console.log('success!!');
-  })
-  .catch((e: Error) => {
-    console.error(`Uncaught exception while running example: ${e.message}`);
-    throw e;
-  });
+  .then(() => console.log('done'))
+  .catch(console.error);

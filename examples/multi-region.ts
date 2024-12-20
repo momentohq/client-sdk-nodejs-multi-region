1596e4594cca1b90efe21fffd9abf225a96adf52
Michael Landis <michael@momentohq.com>
2024-12-20 09:00:28 -0800

    chore: adjust example program and README (#12)
    Reference the example program in the README template and add more
    README detail.

--- examples/multi-region.ts
+++ examples/multi-region.ts
@@ -2,13 +2,12 @@ import {
   Configurations,
   CredentialProvider,
   MultiRegionCacheSetResponse,
-  MultiRegionCacheSortedSetPutElementsResponse,
   MultiRegionCacheWriterClient,
 } from '@gomomento-poc/sdk-multi-region';
 
 async function main(): Promise<void> {
-  const cacheName = process.env.MOMENTO_CACHE_NAME ?? 'test-cache';
   const client = new MultiRegionCacheWriterClient({
+    // Can provide meaningful names for each region
     credentialProviders: {
       'region-1': CredentialProvider.fromEnvVar('MOMENTO_API_KEY_REGION_1'),
       'region-2': CredentialProvider.fromEnvVar('MOMENTO_API_KEY_REGION_2'),
@@ -16,6 +15,7 @@ async function main(): Promise<void> {
     configuration: Configurations.Laptop.latest(),
     defaultTtlSeconds: 60,
   });
+  const cacheName = process.env.MOMENTO_CACHE_NAME ?? 'cache';
 
   const setResponse = await client.set(cacheName, 'scalar', 'value');
   switch (setResponse.type) {
@@ -24,31 +24,18 @@ async function main(): Promise<void> {
       break;
     case MultiRegionCacheSetResponse.Error:
       console.error(
-        `Found {${Object.keys(setResponse.errors()).length}} errors and {${
+        `Found ${Object.keys(setResponse.errors()).length} errors and ${
           Object.keys(setResponse.successes()).length
-        }} successes. The full results are: ${setResponse.toString()}`
+        } successes. Errors:`
       );
-      break;
-  }
-
-  const sortedSetResponse = await client.sortedSetPutElements(cacheName, 'sorted-set', {
-    element1: 1,
-    element2: 2,
-  });
-  switch (sortedSetResponse.type) {
-    case MultiRegionCacheSortedSetPutElementsResponse.Success:
-      console.log('Success:', sortedSetResponse.results());
-      break;
-    case MultiRegionCacheSortedSetPutElementsResponse.Error:
-      console.error(
-        `Found {${Object.keys(sortedSetResponse.errors()).length}} errors and {${
-          Object.keys(sortedSetResponse.successes()).length
-        }} successes. The full results are: ${setResponse.toString()}`
-      );
+      // Handle regional write errors as needed
+      for (const [region, error] of Object.entries(setResponse.errors())) {
+        console.error(`- ${region}: ${error.toString()}`);
+      }
       break;
   }
 }
 
 main()
   .then(() => console.log('done'))
-  .catch(console.error);
+  .catch(err => console.error(err));
